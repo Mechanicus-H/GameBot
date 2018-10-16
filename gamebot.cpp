adacefa2fcbfc86b56b4c9ea2246bccb96d3b3c2
Sergius <g87trader@gmail.com>
2018-10-17 00:51:58 +0300

    17.10.2018 00:51
    Want to sleep

--- gamebot.cpp
+++ gamebot.cpp
@@ -3,9 +3,93 @@
 GameBot::GameBot(QWidget *parent)
     : QWidget(parent)
 {
+    startTimer(2);
+
+    setFixedSize(200, 75);
+
+    lblX=new QLabel;
+    lblY=new QLabel;
+    lblClick=new QLabel;
+
+
+    QHBoxLayout *box=new QHBoxLayout;
+    box->addWidget(lblX);
+    box->addWidget(lblY);
+    box->addWidget(lblClick);
+
+    setLayout(box);
+
+    Qt::WindowFlags flag=Qt::WindowStaysOnTopHint;
+    setWindowFlags(flag);
+
 }
 
 GameBot::~GameBot()
 {
 
 }
+//-----------------------------------------------
+void GameBot::slotSetClick(QTime time)
+{
+    lblClick->setText("Last click: "+ time.toString("hh:mm:ss"));
+}
+//-----------------------------------------------
+void GameBot::slotSetX(int x)
+{
+    lblX->setText("X: " + QString::number(x));
+}
+//-----------------------------------------------
+void GameBot::slotSetY(int y)
+{
+    lblY->setText("Y: " + QString::number(y));
+}
+//-----------------------------------------------
+void GameBot::slotShowMouseState()
+{
+    slotSetX(cursorPosition.x());
+    slotSetY(cursorPosition.y());
+    slotSetClick(lastClick);
+}
+//-----------------------------------------------
+void GameBot::focusOutEvent(QFocusEvent *fe)
+{
+    this->activateWindow();
+    QWidget::focusOutEvent(fe);
+}
+//-----------------------------------------------
+void GameBot::timerEvent(QTimerEvent *te)
+{
+    cursorPosition=cursor().pos();
+
+    slotShowMouseState();
+}
+//-----------------------------------------------
+void GameBot::mouseMoveEvent(QMouseEvent *me)
+{
+    QWidget::mouseMoveEvent(me);
+}
+//-----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- gamebot.h
+++ gamebot.h
@@ -2,14 +2,36 @@
 #define GAMEBOT_H
 
 #include <QWidget>
+#include <QtWidgets>
 
 class GameBot : public QWidget
 {
     Q_OBJECT
+private:
+    QPoint cursorPosition;
+    QTime lastClick;
+    QLabel *lblX;
+    QLabel *lblY;
+    QLabel *lblClick;
 
 public:
     GameBot(QWidget *parent = 0);
     ~GameBot();
+
+    void timerEvent(QTimerEvent *te);
+    void focusOutEvent(QFocusEvent *fe);
+    void mouseMoveEvent(QMouseEvent *me);
+
+    void mousePressEvent(QMouseEvent *me)
+    {
+
+    }
+
+public slots:
+    void slotShowMouseState();
+    void slotSetX(int x);
+    void slotSetY(int y);
+    void slotSetClick(QTime time);
 };
 
 #endif // GAMEBOT_H
